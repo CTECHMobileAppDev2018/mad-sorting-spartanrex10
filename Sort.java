6da1625a67c80dce7c12ae09edf05eaa9f48c0cd
spartanrex10 <46965967+spartanrex10@users.noreply.github.com>
2019-02-13 09:05:53 -0600

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -3,10 +3,66 @@ import java.util.Arrays;
 class Main 
 { 
 
-    // start and end are optional - some sorting algorithms use them, but you can ignore them
-    void sort(double arr[], long start, long end) 
+      /* This function takes last element as pivot, 
+       places the pivot element at its correct 
+       position in sorted array, and places all 
+       smaller (smaller than pivot) to left of 
+       pivot and all greater elements to right 
+       of pivot */
+    int partition(double arr[], int low, int high) 
     { 
-      // sort the array here
+        double pivot = arr[high];  
+        int i = (low-1); // index of smaller element 
+        for (int j=low; j<high; j++) 
+        { 
+            // If current element is smaller than or 
+            // equal to pivot 
+            if (arr[j] <= pivot) 
+            { 
+                i++; 
+  
+                // swap arr[i] and arr[j] 
+                double temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+            } 
+        } 
+  
+        // swap arr[i+1] and arr[high] (or pivot) 
+        double temp = arr[i+1]; 
+        arr[i+1] = arr[high]; 
+        arr[high] = temp; 
+  
+        return i+1; 
+    } 
+  
+  
+    /* The main function that implements QuickSort() 
+      arr[] --> Array to be sorted, 
+      low  --> Starting index, 
+      high  --> Ending index */
+    void sort(double arr[], int low, int high) 
+    { 
+        if (low < high) 
+        { 
+            /* pi is partitioning index, arr[pi] is  
+              now at right place */
+            int pi = partition(arr, low, high); 
+  
+            // Recursively sort elements before 
+            // partition and after partition 
+            sort(arr, low, pi-1); 
+            sort(arr, pi+1, high); 
+        } 
+    } 
+  
+    /* A utility function to print array of size n */
+    static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i]+" "); 
+        System.out.println(); 
     } 
 
   
@@ -25,7 +81,7 @@ class Main
         
        
         Main sorter = new Main(); // create a sorter object
-        sorter.sort(arr, 0, arr.length); // call the sort function
+        sorter.sort(arr, 0, arr.length-1); // call the sort function
   
         System.out.println("*** your sorted array: "); 
         System.out.println(Arrays.toString(arr));
