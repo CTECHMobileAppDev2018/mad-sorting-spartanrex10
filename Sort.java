8395027403d24b9ab4fe76d1f13534af26793022
spartanrex10 <46965967+spartanrex10@users.noreply.github.com>
2019-02-14 08:54:23 -0600

    Fixed some bugs

--- Sort.java
+++ Sort.java
@@ -54,16 +54,7 @@ class Main
             sort(arr, low, pi-1); 
             sort(arr, pi+1, high); 
         } 
-    } 
-  
-    /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i]+" "); 
-        System.out.println(); 
-    } 
+    }
 
   
     // Test program 
